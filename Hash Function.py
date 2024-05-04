c6fcb96ba1c539f80434f852ebde7f9ca7e53887
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-05-04 17:06:52 +0530

    Update Hash Function.py

--- Hash Function.py
+++ Hash Function.py
@@ -1,35 +1,36 @@
 HT = [[] for _ in range(10)]
-print("Blank = ", HT)
+print("Blank = ",HT)
 
-def insert(HT, key, value):
+def insert(HT,key,value):
     loc = key % 10
     bucket = HT[loc]
     exist = False
     for i, kv in enumerate(bucket):
-        k, v = kv
+        k,v = kv
         if key == k:
             exist = True
             break
     if exist:
-        bucket[i] = (key, value)
+        bucket[i] = (key,value)
     else:
-        bucket.append((key, value))
+        bucket.append((key,value))
 
-def delete(HT, key):
+def delete(HT,key):
     loc = key % 10
     bucket = HT[loc]
     exist = False
-    for i, kv in enumerate(bucket):
-        k, v = kv
+    for i,kv in enumerate(bucket):
+        k,v = kv
         if key == k:
             exist = True
             break
     if exist:
         del bucket[i]
+        print("Element is deleted")
     else:
-        print("Element not found to delete")
+        print("Element not found")
 
-def search(HT, key):
+def search(HT,key):
     loc = key % 10
     bucket = HT[loc]
     exist = False
@@ -39,35 +40,36 @@ def search(HT, key):
             exist = True
             break
     if exist:
-        print("Element found, value: ", v)
+        print("Element found, value :",v)
     else:
         print("Element not found")
 
 def display(HT):
     for i, bucket in enumerate(HT):
-        print("Bucket", i, "Elements:", bucket)
+        print("Bucket",i,"Elements:",bucket)
 
-# Main Program
 while True:
-    print("\n1. Insert")
-    print("2. Delete")
-    print("3. Search key")
-    print("4. Print Hash Table")
-    print("0. Exit")
-    ch = int(input("Enter Choice: "))
+    print("\n1.Insert key & Value")
+    print("2.Delete")
+    print("3.Search")
+    print("4.Print Hash Table")
+    print("0.Exit")
+    ch = int(input("Enter Choice :"))
+
     if ch == 1:
-        key = int(input("Enter key: "))
-        value = input("Enter value: ")
+        key = int(input("Enter key to insert :"))
+        value = input("Enter value to insert :")
         insert(HT, key, value)
     elif ch == 2:
-        key = int(input("Enter the key: "))
+        key = int(input("Enter the key to delete :"))
         delete(HT, key)
     elif ch == 3:
-        key = int(input("Enter the key: "))
+        key = int(input("Enter the key to search :"))
         search(HT, key)
     elif ch == 4:
         display(HT)
     elif ch == 0:
         break
     else:
-        print("Wrong choice")
+        print("Wrong Choice")
+        
