56b4ba412169a02ab82a57f652c9cebee1d5750a
Sarthak R. Katare <kataresarthak001@gmail.com>
2024-05-07 16:09:18 +0530

    Update General Tree.cpp

--- General Tree.cpp
+++ General Tree.cpp
@@ -5,99 +5,95 @@ requirements of your method.
 */
 
 #include<iostream>
-#include <string>
+#include<string.h>
 using namespace std;
 
-struct Node
+struct node
 {
-    string name;
+    char label[10];
     int count;
-    Node *child[10];
-};
+    struct node *child[10];
+}*root;
 
 class Tree
 {
     public:
-        Node *root;
-        Tree()
-        {
-            root=NULL;
-        }
-        void createnode();
-        void displaytree();
+    Tree()
+    {
+        root=NULL;
+    }
+    void create();
+    void display(node *r1);
 };
 
-void Tree::createnode()
+void Tree::create()
 {
-    root=new Node;
-    
-    cout<<"ENTER THE NAME OF THE BOOK:- ";
-    cin.ignore();
-    getline(cin,root->name);
-    
-    cout<<"ENTER THE NUMBER OF CHAPTERS IN THE BOOK:- ";
+    int i,j;
+    root=new node;
+    cout<<"ENTER THE NAME OF THE BOOK :";
+    cin>>root->label;
+    cout<<"ENTER THE NUMBER OF CHAPTERS IN THE BOOK :";
     cin>>root->count;
-    
-    for(int i=0;i<root->count;i++)
+    for(i=0;i<root->count;i++)
     {
-        root->child[i]=new Node;
-        cout<<"\tENTER THE NAME OF THE CHAPTER "<<i+1<<":- ";
-        cin.ignore();
-        getline(cin,root->child[i]->name);
-        
-        cout<<"\tENTER NUMBER OF SECTIONS IN CHAPTER "<<root->child[i]->name<<":- ";
+        root->child[i]=new node;
+        cout<<"ENTER THE NAME OF CHAPTER "<<i+1<<" :";
+        cin>>root->child[i]->label;
+        cout<<"ENTER NUMBER OF SECTIONS IN CHAPTER "<<root->child[i]->label<<" :";
         cin>>root->child[i]->count;
-        
-        for(int j=0;j<root->child[i]->count;j++)
+        for (j=0;j<root->child[i]->count;j++)
         {
-            root->child[i]->child[j]=new Node;
-            cout<<"\t\tENTER THE NAME OF THE SECTION "<<j+1<<":- ";
-            cin.ignore();
-            getline(cin,root->child[i]->child[j]->name);
-            
+            root->child[i]->child[j]=new node;
+            cout<<"ENTER THE NAME OF SECTION "<<j+1<<" :";
+            cin>>root->child[i]->child[j]->label;
         }
     }
+
 }
 
-
-void Tree::displaytree()
+void Tree::display(node *r1)
 {
-    cout<<"\n================INDEX================\n";
-    cout<<"BOOK\t\tCHAPTER\t\tSECTION"<<endl;
-    cout<<root->name<<endl;
-    for(int i=0;i<root->count;i++)
+    int i,j,tchapters;
+    if(r1!=NULL)
     {
-        cout<<"\t\t"<<root->child[i]->name<<endl;
-        for(int j=0;j<root->child[i]->count;j++)
+        cout<<"\n--------------INDEX--------------";
+        cout<<"\nBOOK : "<<r1->label;
+        tchapters=r1->count;
+        for(i=0;i<tchapters;i++)
         {
-            cout<<"\t\t\t\t"<<root->child[i]->child[j]->name<<endl;
+            cout<<"\nCHAPTER "<<i+1<<" : ";
+            cout<<r1->child[i]->label;
+            cout<<"\nSECTION : ";
+            for(j=0;j<r1->child[i]->count;j++)
+            {
+                cout<<r1->child[i]->child[j]->label<<" ";
+            }
         }
-        cout<<"\t\t--------------------------------"<<endl;
+        cout<<endl;
     }
 }
 
 int main()
 {
-    int ch;
     Tree t;
+    int ch;
     while(true)
     {
-        cout<<"\n================MENU================\n";
         cout<<"\n1.CREATE TREE"<<endl;
         cout<<"2.DISPLAY TREE"<<endl;
-        cout<<"0.EXIST"<<endl;
+        cout<<"0.EXIT"<<endl;
         cout<<"ENTER YOUR CHOICE :";
         cin>>ch;
-        switch (ch)
+
+        switch(ch)
         {
             case 1:
-                t.createnode();
+                t.create();
                 break;
             case 2:
-                t.displaytree();
+                t.display(root);
                 break;
             case 0:
-                exit(0);
                 break;
             default:
                 cout<<"WRONG CHOICE !!";
